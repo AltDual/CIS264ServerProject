98a215ae8c43d005b785f7c18e1c4c7c63a53afe
MM <mzyacc@outlook.com>
2025-11-12 14:25:29 -0800

    Update reading.py

--- src/reading.py
+++ src/reading.py
@@ -1,8 +1,6 @@
 import json
 import numpy as np
 
-from StopCleaner import min_index
-
 with open('temp.json', 'r') as f:
     temp = json.load(f)
 
@@ -10,12 +8,12 @@ with open('temp.json', 'r') as f:
 busName = '250'
 direction_str = '1'
 begin_time = '093000'
-busPosition_index = 5
 target_stop = 341190
 
 
 # The intermediates
-min_index = None
+stop_coord_index = None
+live_bus_position_index = 5
 
 matching_keys = [k for k in temp[busName][direction_str] if str(k).startswith(begin_time)]
 print(matching_keys)
@@ -31,16 +29,16 @@ stopsArrLen = stopsArr[direction_str][stopsArr.shape[1]-1][0]
 
 for i in range(stopsArrLen):
     if stopsArr[direction_str][i][0] == target_stop:
-        min_index = stopsArr[direction_str][i][3]
+        stop_coord_index = stopsArr[direction_str][i][3]
 
-distance_endpoint = routeA[direction_str][min_index][2]
-distance_startpoint = routeA[direction_str][busPosition_index][2]
+distance_endpoint = routeA[direction_str][stop_coord_index][2]
+distance_startpoint = routeA[direction_str][live_bus_position_index][2]
 
-bus_distance = routeA[direction_str][min_index][2]
-for things in matching_keys:
-    temp_min_index = 0
-    dis_apart = abs(temp[busName][direction_str][things][0][0] - )
-    for i in range(1, len(temp[busName][direction_str][things])):
-        if temp[busName][direction_str][things][i][0]
+# bus_distance = routeA[direction_str][min_index][2]
+# for things in matching_keys:
+#     temp_min_index = 0
+#     dis_apart = abs(temp[busName][direction_str][things][0][0] - )
+#     for i in range(1, len(temp[busName][direction_str][things])):
+#         if temp[busName][direction_str][things][i][0]
 
 
